0902454834aea596508d58e3a6f2e91efd167549
Luke7720 <81709312+0x4c37373230@users.noreply.github.com>
2021-11-24 22:53:10 -0300

    Add files via upload

--- MCMODDLL/mod.cpp
+++ MCMODDLL/mod.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "pch.h"
 #include "mod.h"
 #include "Minecraft.hpp"
@@ -13,7 +14,54 @@ void mod_exit()
 
 }
 
+std::string getArmType(void* pistonThis)
+{
+	std::string result{ "Regular" };
+
+	if (*((BYTE*)pistonThis + 208))
+		result = "Sticky";
+	return result;
+}
+
+void pistonArmState(void* _pistonThis, char state)
+{
+	std::string getArm{ getArmType(_pistonThis) };
+
+	if (state == 'e')
+	{
+		std::cout << getArm << " piston is expanding" << std::endl;
+	}
+	else
+	{
+		std::cout << getArm << " piston is retracting" << std::endl;
+	}
+}
+
+//PistonBlockActor::_checkAttachedBlocks
 THook(char, fFEJoOUAfG, void* _this, BlockSource* a2)
 {
 	return '1';
+}
+
+//PistonBlockActor::isExpanded
+THook(bool, D_LHvgIEZR, void* _this) 
+{
+	std::string getArm{ getArmType(_this) };
+
+	std::cout << getArm << " piston was updated" << std::endl;
+	return original(_this);
+}
+
+//PistonBlockActor::isExpanding
+THook(bool, eDWwWumtrL, void* _this)
+{
+	pistonArmState(_this, 'e');
+	return original(_this);
+}
+
+//PistonBlockActor::isRetracting
+THook(bool, Xq_Bdi_TjC, void* _this)
+{
+	pistonArmState(_this, 'r');
+	return original(_this);
 }
